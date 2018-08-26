cc84b4d075f53dba6fc03697292003948be54d72
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2018-08-26 01:27:24 +0000

    documentation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Iguana
-A lightweight JavaScript library with useful functions and short cuts
+# Iguana [![Build Status](https://travis-ci.com/Paparoni/Iguana.svg?branch=master)](https://travis-ci.com/Paparoni/Iguana)
+>A lightweight JavaScript library with useful functions, custom classes and shortcuts
+
+View Documentation
+
+## Install with npm
+In terminal:
+```shell
+npm install iguana
+```
+In your node js file:
+```javascript
+const iguana = require('iguana');
+const I = new iguana();
+```
+## Install with local HTML & JavaScript
+
+ - Download code found in the `dist` folder
+ - Include this in your HTML file:
+ 
+   ```HTML
+    <script src="iguana.min.js"></script>```
--- src/iguana.js
+++ src/iguana.js
@@ -1,16 +1,63 @@
-(function() {
+    /**
+     * List class
+     * @example
+     * let myList = new Iguana.List(1, 2, "foo", "bar");
+     * console.log(myList[0])
+     * => 1
+     * 
+     */
     class List extends Array {
         constructor() {
             super();
+            /**
+             * List.origin
+             * @desc a List containing all of the elements added at the start of the list
+             * @example
+             * let myList = new Iguana.List(1, 2, "foo", "bar");
+             * myList.push("fizz");
+             * // "fizz" is not printed to the console because it was added to the List after it was defined.
+             * myList.origin.forEach(function(element){ console.log(element) });
+             * => 1
+             * => 2
+             * => "foo"
+             * => "bar"
+             */
+            this.origin = new List();
             for (let i = 0; i < arguments.length; i++) {
                 this[i] = arguments[i];
+                this.origin.push(arguments[i]);
+
             }
         }
-
-        delete(val) {
-            this.splice(this.indexOf(val), 1);
+        /**
+         * This function removes an element from a list
+         * @returns void.
+         * @example
+         * let myList = new Iguana.List(1, 2, "foo", "bar");
+         * myList.delete("bar");
+         * myList.forEach(function(element){ console.log(element) });
+         * => 1
+         * => 2
+         * => "foo"
+         */
+        delete(element) {
+            this.splice(this.indexOf(element), 1);
         }
 
+        /**
+         * This function returns a random element from a List.
+         * @param {number} num optional parameter, if given this function will return a new List with randomized elements this parameter will define the number of elements that will be in the shuffled list.
+         * @returns {any}
+         * @returns {List}
+         * @example
+         * let myList = new Iguana.List(1, 2, "foo", "bar", "fizz", 3);
+         * console.log(myList.shuffle())
+         * => "foo"
+         * // If shuffle is given a number as a parameter
+         * // This will return a new a list with 3 random elements from the original list
+         * console.log(myList.shuffle(3))
+         * => List ["fizz", 1, "bar"]
+         */
         shuffle(num) {
 
             if (arguments.length === 0) {
@@ -84,6 +131,4 @@
         }
     }
 
-    module.exports = Iguana;
-
-})();
+module.exports = Iguana;
